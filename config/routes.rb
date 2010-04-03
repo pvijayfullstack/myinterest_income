cb91ee98d914500247bcbe59bd686057d4de01f5
Rohit Sachdeva <growth@drinnovations.us>
2010-04-03 10:35:42 -0500

    Updating ReadMe Approaches

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :customers
+
 
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -33,8 +33,10 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "customers"
 
+  map.resources :customers
+  
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
