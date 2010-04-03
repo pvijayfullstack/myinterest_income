40d535e5447fe5bc5b228151a64fb202e38ac2e9
Rohit Sachdeva <growth@drinnovations.us>
2010-04-03 09:50:54 -0500

    Initial New Investment Approaches

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,10 @@ module ApplicationHelper
   #the new_object in this case is investment. Could rather be created later also.
 
   #Alternative, let JavaScript call without inline. And show up values directly similar to above.
-  # AT submission time the similar investment object can be created as required.
 
+  # In the end, the params[:customer] should be something like
+  #"name"=>"cust1", "investments_attributes"=>{"1626"=>{"investment_amount"=>"273", "investment_name"=>"inv1", "investment_apy"=>"8"}, "1627"=>{"investment_amount"=>"818", "investment_name"=>"inv2", "investment_apy"=>"9"}}
+  #here 1626 and 1627 are just dummy
   #Under investigation the approaches here.
   def link_to_add_fields(name, f, association)
     new_object = f.object.class.reflect_on_association(association).klass.new
