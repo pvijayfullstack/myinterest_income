e5a1ccb6f639888ce15c0e4498f398cf9908d077
Rohit Sachdeva <growth@drinnovations.us>
2010-04-03 23:36:34 -0500

    Adding jQuery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+/*
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
   $(link).up().insert({
     before: content.replace(regexp, new_id)
   });
+}
+*/
+
+
+function add_fields(link, association, content) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g")
+  $(link).parent().before(content.replace(regexp, new_id));
 }
