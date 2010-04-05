b5b1021172a476760b0df4ceaf6e7f0cbcdc6013
Rohit Sachdeva <growth@drinnovations.us>
2010-04-04 20:16:09 -0500

    Handling case when customer has no investments and we click edit. Shows a new blank investment

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,20 +9,17 @@
 
         $('a.new_investment').live('click', function (e){
             var x = ($('#initial').clone());
-            console.log("checking here");
-            x.find('a').attr('value','');
             x = x.html();
             var new_id = new Date().getTime();
             var regexp = new RegExp("start", "g");
-            x = x.replace(regexp, new_id);
+            var replaced  = x.replace(regexp, new_id);
             regexp = new RegExp("display: none;", "g");
-            x = x.replace(regexp, "");
-            $(x).appendTo('#new_investments');
+            replaced = replaced.replace(regexp, "");
+            $(replaced).find('input').attr('value','').end().appendTo('#new_investments');
             e.preventDefault();
         });
 
         $('.delete_investment').live('click', function (e){
-            console.log("$(this) ", $(this));
             $(this).prev("input[type=hidden]").val("1");
             $(this).closest(".specific_investment").hide();
             return false;
