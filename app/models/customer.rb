0e9f1cd3733a1d47a67159c2076d7acfe171d714
Rohit Sachdeva <growth@drinnovations.us>
2010-04-05 16:39:06 -0500

    Getting started with Basic Calculations in Investment Model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,4 @@
 class Customer < ActiveRecord::Base
   has_many :investments, :dependent => :destroy
   accepts_nested_attributes_for :investments, :allow_destroy => true
-
-  def average_monthly
-    
-  end
 end
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,3 +1,7 @@
 class Investment < ActiveRecord::Base
   belongs_to :customer
+
+  def average_monthly
+    investment_amount*investment_years
+  end
 end
