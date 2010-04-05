463e7c69bfb4808040b995bf9f6284567a3490fb
Rohit Sachdeva <growth@drinnovations.us>
2010-04-05 17:21:46 -0500

    Calculations in Investment Model

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,7 +1,16 @@
 class Investment < ActiveRecord::Base
   belongs_to :customer
 
-  def average_monthly
-    investment_amount*investment_years
+  def compound_interest_years
+    apy = investment_apy/100
+    calculated_result_partial = (1+apy)**investment_years
+    (investment_amount*calculated_result_partial)-investment_amount
   end
+
+
+  def average_interest_monthly
+     interest_per_day = (compound_interest_years)/(investment_years*365)
+     interest_per_day*30
+  end
+  
 end
