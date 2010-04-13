a50e881f75dc75891ad327a587e44179637ce8f8
Rohit Sachdeva <growth@drinnovations.us>
2010-04-13 11:29:34 -0500

    Comments added

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,6 +1,9 @@
 class Bank < ActiveRecord::Base
   has_many :investments
 
+  validates_presence_of :name
+  validates_uniqueness_of :name
+
 
   #to avoid any race conditions lets first try to create and then use find
   def self.create_or_find_by_name(name)
